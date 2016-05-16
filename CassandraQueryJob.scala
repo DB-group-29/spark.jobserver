371e9f5a2579f36a2396fa4d879f52346701d77b
Viktor Yberg <Viktor Yberg>
2016-05-16 03:05:15 +0200

    Updates

--- CassandraQueryJob.scala
+++ CassandraQueryJob.scala
@@ -1,60 +1,74 @@
 package spark.jobserver
 
-import com.typesafe.config.{Config, ConfigFactory}
-import org.apache.spark._
-import org.apache.spark.SparkContext._
-import spark.jobserver._
-import com.datastax.spark.connector._
+import com.typesafe.config.{Config, ConfigException}
 import org.apache.spark.sql.cassandra.CassandraSQLContext
-import scala.util.Try
 
-object CassandraQuery extends SparkCassandraJob {
+trait CassandraQuery extends SparkCassandraJob {
+  def validate(cc: CassandraSQLContext, config: Config): SparkJobValidation = SparkJobValid
+}
+
+object GetSubreddits extends CassandraQuery {
 
   override def runJob(cc: CassandraSQLContext, config: Config): Any = {
-    // Assuming your request sent a { "query": "..." } in the body:
-    val df = cc.sql(config.getString("query"))
-    df.map(_.getValuesMap[Any](List("created_utc", "subreddit", "subreddit_id", "link_id", "id", "parent_id", "name", "author", "body", "ups", "downs", "score"))).collect
-    // createResponseFromDataFrame(df) // You should implement this
-  }
-  override def validate(cc: CassandraSQLContext, config: Config): SparkJobValidation = {
-    // Code to validate the parameters received in the request body
-    // Try(config.getString("query"))
-    //  .map(x => SparkJobValid)
-    //  .getOrElse(SparkJobInvalid("No 'query' given"))
-    SparkJobValid
+    cc.sql("SELECT DISTINCT subreddit FROM reddit.comments")
+      .map(_.getValuesMap[Any](List("subreddit"))).collect
   }
 }
 
-object GetSubreddits extends SparkCassandraJob {
+object GetSubredditsDistinct extends CassandraQuery {
 
   override def runJob(cc: CassandraSQLContext, config: Config): Any = {
-    // Assuming your request sent a { "query": "..." } in the body:
-    val df = cc.sql("SELECT subreddit, subreddit_id FROM reddit.comments").distinct()
-    df.map(_.getValuesMap[Any](List("subreddit", "subreddit_id"))).collect
-    // createResponseFromDataFrame(df) // You should implement this
-  }
-  override def validate(cc: CassandraSQLContext, config: Config): SparkJobValidation = {
-    // Code to validate the parameters received in the request body
-    // Try(config.getString("query"))
-    //  .map(x => SparkJobValid)
-    //  .getOrElse(SparkJobInvalid("No 'query' given"))
-    SparkJobValid
+    cc.sql("SELECT subreddit FROM reddit.comments").distinct()
+      .map(_.getValuesMap[Any](List("subreddit"))).collect
   }
 }
 
-object GetThreads extends SparkCassandraJob {
+object GetThreads extends CassandraQuery {
 
   override def runJob(cc: CassandraSQLContext, config: Config): Any = {
-    // Assuming your request sent a { "query": "..." } in the body:
-    val df = cc.sql("SELECT link_id FROM reddit.comments").distinct()
-    df.map(_.getValuesMap[Any](List("link_id"))).collect
-    // createResponseFromDataFrame(df) // You should implement this
+    var rows = 20
+    var query: String = "SELECT link_id FROM reddit.comments "
+    try {
+      val subreddit: String = config.getString("subreddit")
+      query += " WHERE subreddit = '" + subreddit + "'"
+    } catch { case e: ConfigException => }
+    try {
+      val page: Int = config.getString("page").toInt
+      rows *= page
+    } catch { case e: ConfigException => }
+    query += " LIMIT " + rows.toString
+
+    cc.sql(query).distinct()
+      .map(_.getValuesMap[Any](List("link_id"))).collect
   }
-  override def validate(cc: CassandraSQLContext, config: Config): SparkJobValidation = {
-    // Code to validate the parameters received in the request body
-    // Try(config.getString("query"))
-    //  .map(x => SparkJobValid)
-    //  .getOrElse(SparkJobInvalid("No 'query' given"))
-    SparkJobValid
+}
+
+object GetComments extends CassandraQuery {
+
+  override def runJob(cc: CassandraSQLContext, config: Config): Any = {
+    var query: String = "SELECT created_utc, subreddit, parent_id, name, author, body, ups, downs, score FROM reddit.comments"
+    try {
+      val link_id: String = config.getString("link_id")
+      query += " WHERE link_id = '" + link_id + "'"
+    } catch { case e: ConfigException => }
+
+    cc.sql(query)
+      .map(_.getValuesMap[Any](List("created_utc", "subreddit", "parent_id", "name",
+        "author", "body", "ups", "downs", "score"))).collect
+  }
+}
+
+object GetUser extends CassandraQuery {
+
+  override def runJob(cc: CassandraSQLContext, config: Config): Any = {
+    var query: String = "SELECT created_utc, subreddit, link_id, parent_id, name, author, body, ups, downs, score FROM reddit.comments"
+    try {
+      val author: String = config.getString("author")
+      query += " WHERE author = '" + author + "'"
+    } catch { case e: ConfigException => }
+
+    cc.sql(query)
+      .map(_.getValuesMap[Any](List("created_utc", "subreddit", "link_id", "parent_id", "name",
+        "author", "body", "ups", "downs", "score"))).collect
   }
 }
